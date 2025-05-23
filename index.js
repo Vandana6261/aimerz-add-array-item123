3bb8e4a2ead3e02897d22b7ba4a529572f265997
Vandana <vvandanapatidar992@gmail.com>
2025-05-23 19:41:36 +0530

    readme file added

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function addArrayItems(arr){
     return arr.reduce((acc, data) => acc + data)
 }
 module.exports = addArrayItems
+// This is for export multiple function.
+// module.exports = {addArrayItems, deleteArrayItems}
